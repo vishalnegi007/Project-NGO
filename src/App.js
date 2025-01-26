e9a76d6cf878a7b6042e005630b310cf863f69ff
Vishal Negi <vishal.negi@cvsciences.com>
2025-01-26 20:53:15 +0530

    Home js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contactus" element={<ContactUs />} />
       </Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
     </div>
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contactus">Contact</Link></li>
       </ul>
